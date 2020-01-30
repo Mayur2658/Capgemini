dc937eb92bc7216bc9e94a040b1bf20fe73c9882
Mayur Borkar <mayurborkar20@gmail.com>
2020-01-30 16:47:55 +0530

    first commit

---
+++ Person.java
@@ -0,0 +1,16 @@
+package Lab2_1;
+// Write a java program to print person details in the format as shown below: 
+public class Person {
+
+	public static void main(String[] args) {
+		System.out.println("Person Details: ");
+		System.out.println("_______________\n");
+		System.out.println("First Name: Divya");
+		System.out.println("Last Name: Bharathi");
+		System.out.println("Gender: F");
+		System.out.println("Age: 20");
+		System.out.println("Weight: 85.55");
+
+	}
+
+}
